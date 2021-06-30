302d0639adcef70dd266bd0f4f744298aca01c93
iwata-n <a.tokai.wain@gmail.com>
2021-06-30 13:52:33 +0900

    実装を整理

--- repospots.py
+++ repospots.py
@@ -1,17 +1,18 @@
 import argparse
-from typing import List
+from typing import Dict, List
 from git.objects.commit import Commit
 import yaml
 import json
 import fnmatch
-from git import Repo, config
+from git import Repo
 
 class CommitFile():
     def __init__(self, path: str) -> None:
         self.path = path
         self._commits = []
         self._authors = []
-        self._large_commit = {}
+        self._diff = {}
+        self._large_diff = {}
 
     def to_dict(self):
         return {
@@ -19,18 +20,16 @@ class CommitFile():
             'authors': self.authors(),
             'commit_count': self.commit_count(),
             'author_count': self.author_count(),
-            'large_commit_count': len(self._large_commit),
-            'large_commit': self._large_commit,
-            #'commit': {c.hexsha: {'total': c.stats.total} for c in self._commits},
-            'risk': self.risk(),
+            'diff': self._diff,
         }
 
-    def add_commit(self, commit: Commit):
+    def add_commit(self, commit: Commit, diff: Dict):
         self._commits.append(commit)
         self._authors.append(commit.author.name)
-
-    def add_large_commit(self, commit: Commit):
-        self._large_commit[commit.hexsha] = commit.stats.total['lines']
+        self._diff[commit.hexsha] = {
+            'datetime': commit.authored_datetime.strftime("%Y/%m/%d %H:%M:%S"),
+            'diff': diff
+        }
 
     def authors(self) -> List:
         return list(set(self._authors))
@@ -41,16 +40,13 @@ class CommitFile():
     def author_count(self) -> int:
         return len(self.authors())
 
-    def risk(self) -> float:
-        return self.commit_count() * self.author_count()
-
 class CommitFileJSONEncoder(json.JSONEncoder):
     def default(self, o):
         if isinstance(o, CommitFile):
             return o.to_dict()
         return super().default(o)
 
-def parse_repo(path, branch, depth, exclude, large_commit_lines):
+def parse(path, branch, depth, exclude):
     repo = Repo(path)
 
     commits = repo.iter_commits(branch, max_count=depth)
@@ -69,8 +65,6 @@ def parse_repo(path, branch, depth, exclude, large_commit_lines):
 
         total_commits += 1
 
-        is_large_commit = commit.stats.total['lines'] > large_commit_lines
-
         author = commit.author.name
 
         if author in authors:
@@ -78,26 +72,26 @@ def parse_repo(path, branch, depth, exclude, large_commit_lines):
         else:
             authors[author] = 1
 
-        for file in commit.stats.files:
-            if any([fnmatch.fnmatch(file, e) for e in exclude]):
+        for file in commit.stats.files.items():
+            file_path = file[0]
+            file_diff = file[1]
+
+            if any([fnmatch.fnmatch(file_path, e) for e in exclude]):
                 continue
 
-            if file not in files:
-                files[file] = CommitFile(file)
+            if file_path not in files:
+                files[file_path] = CommitFile(file_path)
 
-            files[file].add_commit(commit)
-
-            if is_large_commit:
-                files[file].add_large_commit(commit)
+            files[file_path].add_commit(commit, file_diff)
 
     return total_commits, files, authors
 
-def load_config(path):
+def _load_config(path):
     with open(path, 'r') as yml:
         config = yaml.safe_load(yml)
     return config
 
-def argument():
+def _argument():
     parser = argparse.ArgumentParser(description='Find bugspot in git repo.')
     parser.add_argument('path', default='./', type=str, help='repository path')
     parser.add_argument('--branch', default='master', dest='branch', type=str)
@@ -106,32 +100,32 @@ def argument():
     parser.add_argument('--top', default=-1, dest='top', type=int)
     parser.add_argument('--member', nargs='+', dest='member', default=[], action='append', type=str)
     parser.add_argument('--config', dest='config', default='', type=str)
+    parser.add_argument('--output', dest='output', default='', type=str)
     parser.add_argument('--show-summary', dest='show_summary', default=True, type=bool)
 
     return parser.parse_args()
 
-def print_summary(result):
-    print("summary")
-    print(f" total commits={result['total_commits']}")
-    print(f" total files={result['total_files']}")
-    print(f" authors={result['authors']}")
-    print('')
-
 def main():
-    args = argument()
+    args = _argument()
     path = args.path
     branch = args.branch
     depth = args.depth
     top = args.top
+    is_debug = False
+    output_file = args.output
+    output_console = True
     exclude = sum(args.exclude, [])
     member = sum(args.member, [])
 
     if args.config:
-        config = load_config(args.config)
+        config = _load_config(args.config)
         branch = config['parse']['branch']
         depth = config['parse']['depth']
         large_commit_lines = config['parse']['large_commit_lines']
         top = config['output']['top']
+        output_file = config['output']['file']
+        output_console = config['output']['console']
+        is_debug = config['debug']
         exclude = config['parse']['file']['exclude']
         member = config['parse']['member']
 
@@ -145,16 +139,18 @@ def main():
         'member': member
     }
 
-    print("config")
-    print(f" path={path}")
-    print(f" branch={branch}")
-    print(f" depth={depth}")
-    print(f" top={top}")
-    print(f" exclude={exclude}")
-    print(f" member={member}")
-    print('')
+    if is_debug:
+        print("config")
+        print(f" path={path}")
+        print(f" branch={branch}")
+        print(f" depth={depth}")
+        print(f" top={top}")
+        print(f' output_file={output_file}')
+        print(f" exclude={exclude}")
+        print(f" member={member}")
+        print('')
 
-    total_commits, files, authors = parse_repo(path, branch, depth, exclude, large_commit_lines)
+    total_commits, files, authors = parse(path, branch, depth, exclude)
 
     result = {
         'total_commits': total_commits,
@@ -163,19 +159,22 @@ def main():
         'files': files,
     }
 
-    # print_summary(result)
-    # print_order_by_number_of_authors(files, top)
-    # print_order_by_change_count(files, top)
-    # print_no_employee(files, employee)
-    # for r in ranking:
-    #     print(r)
+    json_result = json.dumps(
+        {
+            'parameter': parameter,
+            'result': result
+        },
+        sort_keys=True,
+        indent=2,
+        cls=CommitFileJSONEncoder
+    )
 
-    # o = only_one_author(files)
-    # a = filter(lambda x: 'iwata-n' in x[1]['author'], o)
-    # print(json.dumps(list(a), sort_keys=True, indent=2))
+    if output_console:
+        print(json_result)
 
-    #print(result)
-    print(json.dumps({'parameter': parameter, 'result': result}, sort_keys=True, indent=2, cls=CommitFileJSONEncoder))
+    if output_file is not None:
+        with open(output_file, 'w') as f:
+            f.write(json_result)
 
 if __name__ == '__main__':
     main()
